aeb922a2aeb82c6e226d2e81f4c91fce66495df4
AustinAdidas24 <raul090977@gmail.com>
2025-03-04 15:48:14 -0600

    Lab-1
    Getting started with python

---
+++ Lab-1.py
@@ -0,0 +1,7 @@
+# this greets the user by name
+name = input("What is your name? ") #get user input
+print("Hello, " + name) #concatenate the greeting "Hello and users input
+greeting = "Hello World"
+print(greeting.upper)
+print(greeting.lower)
+print(greeting.replace("World", "Python")) # Replacing the words
